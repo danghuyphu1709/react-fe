90bb536516874dd90d0fd98b462f8ba35bfbc4ff
phudh <phudh@lifetex.vn>
2025-03-18 15:36:07 +0700

    css board car items

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,14 @@
 import { Height } from "@mui/icons-material";
 import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
+const APP_BAR_HEIGHT = "58px";
+const BOARD_BAR_HEIGHT = "58px";
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`;
 
 const theme = extendTheme({
   custom: {
-    appBarHeigth: "58px",
-    boarBarHeigth: "60px",
-    textColorDefault: "#ff5252",
+    appBarHeigth: BOARD_BAR_HEIGHT,
+    boarBarHeigth: BOARD_BAR_HEIGHT,
+    BoardContentHeigth: BOARD_CONTENT_HEIGHT,
   },
   colorSchemes: {
     light: {
@@ -17,6 +20,13 @@ const theme = extendTheme({
     },
   },
   components: {
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          ".&:MuiTypography-body1": { fontSize: "0.875rem" },
+        },
+      },
+    },
     MuiCssBaseline: {
       styleOverrides: {
         body: {
@@ -25,12 +35,12 @@ const theme = extendTheme({
             height: "5px",
           },
           "*::-webkit-scrollbar-thumb ": {
-            backgroundColor:'#EEEEEE',
-            borderRadius:'5px'
+            backgroundColor: "#EEEEEE",
+            borderRadius: "5px",
           },
           "*::-webkit-scrollbar-thumb:hover ": {
-            backgroundColor:'#888888',
-            borderRadius:'5px'
+            backgroundColor: "#888888",
+            borderRadius: "5px",
           },
         },
       },
